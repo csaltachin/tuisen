7b2dc3363c94a142cafeadb06f6cdc8d49b740ec
Carlos Solano <csaltachin@gmail.com>
2024-04-11 15:51:02 -0600

    Maintain chat width and height in AppState, and refactor scroll state updating

--- src/main.rs
+++ src/main.rs
@@ -31,31 +31,10 @@ const DEFAULT_CHANNEL: &str = "forsen";
 
 // TODO: Break off ui stuff into its own module
 
-#[derive(Clone)]
 enum ScrollState {
     Bottom,
-    Offset(isize),
+    Offset(usize),
     Top,
-}
-
-impl ScrollState {
-    fn clipped(&self, hi: usize) -> Self {
-        let ihi = hi as isize;
-        match self {
-            // If offset is negative, then we were scrolling down from the top
-            ScrollState::Offset(ref n) if *n < 0 => ScrollState::Offset(ihi + *n),
-            // If offset goes over hi, then we were trying to scroll up. If hi is 0, then we should
-            // stay at the bottom; otherwise we clip at the top
-            ScrollState::Offset(ref n) if *n > ihi => {
-                if ihi == 0 {
-                    ScrollState::Bottom
-                } else {
-                    ScrollState::Top
-                }
-            }
-            state => state.clone(),
-        }
-    }
 }
 
 enum InputMode {
@@ -69,16 +48,72 @@ struct App {
     scroll_active: bool,
     input_field: String,
     input_mode: InputMode,
+    chat_width: u16,
+    chat_height: u16,
 }
 
 impl App {
-    fn init() -> Self {
+    fn init(init_width: u16, init_height: u16) -> Self {
+        // TODO: do we want to compute chat_width and chat_height via the render
+        // layout/constraints? What we have here is correct but hardcoded
         App {
             chat_lines: Vec::new(),
             scroll_state: ScrollState::Bottom,
             scroll_active: false,
             input_field: String::new(),
             input_mode: InputMode::Normal,
+            // Subtract 2 from the left/right borders
+            chat_width: init_width.saturating_sub(2),
+            // Subtract 2 for the top/bottom borders, and 3 for the initial input area height
+            chat_height: init_height.saturating_sub(5),
+        }
+    }
+
+    fn get_scroll_offset_limit(&self) -> usize {
+        self.chat_lines
+            .len()
+            .saturating_sub(self.chat_height.into())
+    }
+
+    fn refresh_chat_size(&mut self, new_chat_width: u16, new_chat_height: u16) {
+        if self.chat_width != new_chat_width {
+            self.chat_width = new_chat_width;
+        }
+        // Update height and adjust scroll state
+        if self.chat_height != new_chat_height {
+            let old_height = self.chat_height.clone();
+            let lines = self.chat_lines.len();
+            self.chat_height = new_chat_height;
+            self.scroll_state = match self.scroll_state {
+                // If there is no overflow anymore, reset scroll state to the initial state (Bottom
+                // with scroll inactive)
+                _ if lines <= (new_chat_height as usize) => {
+                    self.scroll_active = false;
+                    ScrollState::Bottom
+                }
+                // If we were in Bottom or Top, stay there
+                ScrollState::Bottom => ScrollState::Bottom,
+                ScrollState::Top => ScrollState::Top,
+                // Otherwise, we'll try to fix the topmost displayed line. That is, we'll try to
+                // preserve the number of hidden lines above, which is (line count - offset -
+                // chat height).
+                // Basically, if chat height changes by delta, then offset must change by -delta.
+                // Then the new scroll state will be Offset(new offset), unless the new offset is 0
+                // or negative, in which case we just set it to Bottom.
+                // Notice that, at this point, we can assume there is overflow (otherwise we would
+                // have pattern matched earlier). So in the Bottom case, we don't need to disable
+                // scrolling.
+                ScrollState::Offset(n) => {
+                    let old_lines_above = lines.saturating_sub(n + (old_height as usize));
+                    let new_offset =
+                        lines.saturating_sub(old_lines_above + (new_chat_height as usize));
+                    if new_offset > 0 {
+                        ScrollState::Offset(new_offset)
+                    } else {
+                        ScrollState::Bottom
+                    }
+                }
+            }
         }
     }
 }
@@ -92,9 +127,10 @@ fn main() -> io::Result<()> {
     // Init backend and TUI
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    let (init_width, init_height) = terminal.size().map(|rect| (rect.width, rect.height))?;
 
     // App goes here
-    let app = App::init();
+    let app = App::init(init_width, init_height);
     let app_result = run_app(app, &mut terminal);
 
     // Clean up buffer
@@ -215,17 +251,34 @@ fn run_app<B: Backend>(mut app: App, terminal: &mut Terminal<B>) -> io::Result<(
                             app.input_mode = InputMode::Insert;
                         }
                         KeyCode::Up if app.scroll_active => {
+                            let offset_limit = app.get_scroll_offset_limit();
                             app.scroll_state = match app.scroll_state {
-                                ScrollState::Bottom => ScrollState::Offset(1),
-                                ScrollState::Offset(n) => ScrollState::Offset(n + 1),
                                 ScrollState::Top => ScrollState::Top,
+                                // Make sure we convert any Offset(offset_limit) into Top
+                                ScrollState::Bottom => {
+                                    if offset_limit == 1 {
+                                        ScrollState::Top
+                                    } else {
+                                        ScrollState::Offset(1)
+                                    }
+                                }
+                                ScrollState::Offset(n) if n + 1 == offset_limit => ScrollState::Top,
+                                ScrollState::Offset(n) => ScrollState::Offset(n + 1),
                             };
                         }
                         KeyCode::Down if app.scroll_active => {
+                            let offset_limit = app.get_scroll_offset_limit();
                             app.scroll_state = match app.scroll_state {
+                                // Make sure we convert any Offset(0) into Bottom
                                 ScrollState::Bottom | ScrollState::Offset(1) => ScrollState::Bottom,
                                 ScrollState::Offset(n) => ScrollState::Offset(n - 1),
-                                ScrollState::Top => ScrollState::Offset(-1),
+                                ScrollState::Top => {
+                                    if offset_limit == 1 {
+                                        ScrollState::Bottom
+                                    } else {
+                                        ScrollState::Offset(offset_limit - 1)
+                                    }
+                                }
                             };
                         }
                         KeyCode::Home if app.scroll_active => {
@@ -278,19 +331,18 @@ fn render_ui(frame: &mut Frame, app: &mut App) {
         .split(frame.size());
 
     let chat_area = main_areas[0];
-
-    let chat_inner_height = (chat_area.height - 2) as usize;
     let chat_line_count = app.chat_lines.len();
+
+    let chat_inner_width = (chat_area.width - 2) as usize;
+    let chat_inner_height = (chat_area.height - 2) as usize;
+
+    // If the current chat size doesn't match the size in our AppState, update it
+    app.refresh_chat_size(chat_inner_width as u16, chat_inner_height as u16);
 
     // If scroll is not active yet, check for overflow
     if !app.scroll_active && chat_line_count > chat_inner_height {
         app.scroll_active = true;
     }
-
-    // Trim scroll offset if necessary
-    app.scroll_state = app
-        .scroll_state
-        .clipped(chat_line_count.saturating_sub(chat_inner_height));
 
     let chat_lines = match app.scroll_state {
         ScrollState::Bottom => {
@@ -298,11 +350,9 @@ fn render_ui(frame: &mut Frame, app: &mut App) {
             app.chat_lines.get(lo..).unwrap().to_vec()
         }
         ScrollState::Offset(offset) => {
-            // At this point, scroll_state has already been trimmed, so offset should be between 0
-            // and (chat_line_count - chat_inner_height) inclusive. Otherwise something went wrong
-            // and we panic
-            let uoffset: usize = offset.try_into().unwrap();
-            let lo = chat_line_count - chat_inner_height - uoffset;
+            // At this point, offset should be strictly smaller than (chat_line_count -
+            // chat_inner_height). Otherwise, something went wrong and we panic
+            let lo = chat_line_count - chat_inner_height - offset;
             app.chat_lines
                 .get(lo..lo + chat_inner_height)
                 .unwrap()
